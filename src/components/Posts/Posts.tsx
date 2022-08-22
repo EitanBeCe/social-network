0bb833e56e4aa3ef79e7ffc5b2678523c596a976
Eitan <eitanleviberger@gmail.com>
2022-08-23 00:07:23 +0300

    posts changes

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
 import classes from './Posts.module.css';
@@ -6,17 +7,35 @@ import SinglePost from './SinglePost';
 const DUMMY_POSTS = [
   {
     title: 'Post 1',
-    id: 1,
+    id: 'p1',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorum ipsam doloremque incidunt architecto repellendus, sit reprehenderit fuga nam. Voluptate amet quasi repudiandae nisi sapiente dolore veniam consequatur, iusto expedita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorum ipsam doloremque incidunt architecto repellendus, sit reprehenderit fuga nam. Voluptate amet quasi repudiandae nisi sapiente dolore veniam consequatur, iusto expedita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorum ipsam doloremque incidunt architecto repellendus, sit reprehenderit fuga nam. Voluptate amet quasi repudiandae nisi sapiente dolore veniam consequatur, iusto expedita.',
   },
   {
     title: 'Post 2',
-    id: 2,
+    id: 'p2',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorum ipsam doloremque incidunt architecto repellendus, sit reprehenderit fuga nam. Voluptate amet quasi repudiandae nisi sapiente dolore veniam consequatur, iusto expedita.',
+  },
+  {
+    title: 'Post 3',
+    id: 'p3',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorum ipsam doloremque incidunt architecto repellendus, sit reprehenderit fuga nam. Voluptate amet quasi repudiandae nisi sapiente dolore veniam consequatur, iusto expedita.',
   },
 ];
 
 const Posts: React.FC = () => {
+  // List of posts
+  const [posts, setPosts] = useState(DUMMY_POSTS);
+  const [modalOpen, setModalOpen] = useState(false); // Modal if user pressed 'Delete post'
+
+  // *************** https://stackoverflow.com/questions/32658604/react-deleting-from-middle-of-list-removes-last-element-instead
+
+  // *************** WHATS id type now after firebase?
+  const deletePostHandler = (id: number | string) => {
+    setPosts((prev) => prev.filter((post) => post.id !== id));
+
+    setModalOpen(false);
+  };
+
   const dummy = () => {};
   return (
     <Card className={classes.posts}>
@@ -27,8 +46,16 @@ const Posts: React.FC = () => {
         </Button>
       </div>
 
-      {DUMMY_POSTS.map((p, i) => (
-        <SinglePost key={i} title={p.title} text={p.text} />
+      {posts.map((p) => (
+        <SinglePost
+          key={p.id}
+          id={p.id}
+          title={p.title}
+          text={p.text}
+          deletePost={deletePostHandler.bind(null, p.id)}
+          modalOpen={modalOpen}
+          setModalOpen={setModalOpen}
+        />
       ))}
     </Card>
   );
--- src/components/Posts/SinglePost.tsx
+++ src/components/Posts/SinglePost.tsx
@@ -1,27 +1,84 @@
-import React from 'react';
 import classes from './SinglePost.module.css';
+import ReactModal from 'react-modal';
+
 import Card from '../UI/Card/Card';
 import Button from '../UI/Button/Button';
 
 interface Props {
   title: string;
   text: string;
+  id: number | string;
+  deletePost: () => void;
+  modalOpen: boolean;
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-// **************** MUST ADD alert for deleting post
+// **************** DEL post
 
-const SinglePost: React.FC<Props> = ({ text, title }) => {
-  const dummy = () => {};
+const SinglePost: React.FC<Props> = ({
+  text,
+  title,
+  deletePost,
+  modalOpen,
+  setModalOpen,
+}) => {
+  const openModalHandler = () => {
+    setModalOpen(true);
+  }; // Need to lift up
+  const closeModalHandler = () => {
+    setModalOpen(false);
+  };
+
+  // *************** RIGHT HERE)))
+  const editPostHandler = () => {};
+
   return (
     <Card className={classes['single-post']}>
       <h2>{title}</h2>
       <div>{text}</div>
       <div className={classes['post-footer']}>
-        <h5>like</h5>
-        <Button onClick={dummy} className={classes.btn}>
+        <h5>LIKE</h5>
+        <Button onClick={editPostHandler} className={classes.btn}>
+          Edit
+        </Button>
+        <Button onClick={openModalHandler} className={classes.btn}>
           Delete
         </Button>
       </div>
+
+      <ReactModal
+        closeTimeoutMS={500}
+        isOpen={modalOpen}
+        onRequestClose={closeModalHandler} // To close on overlay
+        contentLabel="Modal"
+        ariaHideApp={true}
+        style={{
+          overlay: { backgroundColor: 'rgb(0, 0, 0, 0.4)' },
+          content: {
+            padding: '2rem 3rem',
+            margin: '1rem auto',
+            boxShadow: '0 2px 8px rgba(0, 0, 0, 0.26)',
+            maxWidth: '40rem',
+            borderRadius: '10px',
+            textAlign: 'start',
+            color: '#fff',
+            background: '#372c31',
+            bottom: 'auto',
+            top: '20vh',
+          },
+        }}
+      >
+        <h2>Are you sure you want to delete the post?</h2>
+
+        <div className={classes['modal-footer']}>
+          <Button onClick={deletePost} className={classes.btn}>
+            Delete
+          </Button>
+          <Button onClick={closeModalHandler} className={classes.btn}>
+            Close
+          </Button>
+        </div>
+      </ReactModal>
     </Card>
   );
 };
