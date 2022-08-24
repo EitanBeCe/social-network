34d4e267f7924ec80343011e0608405a62c49c2f
Eitan <eitanleviberger@gmail.com>
2022-08-24 23:20:20 +0300

    text change

--- src/components/helpers/dummyPosts.tsx
+++ src/components/helpers/dummyPosts.tsx
@@ -4,19 +4,19 @@ export const DUMMY_POSTS = [
   {
     title: 'Post 1',
     id: uuidv4(),
-    text: 'MICHOEL IS THE BEST!!!!',
+    text: 'React is the best!!!!',
     likes: 16,
   },
   {
     title: 'Post 2',
     id: uuidv4(),
-    text: 'Nathan is a technical boss',
+    text: 'TS and tsx are important',
     likes: 17,
   },
   {
     title: 'Post 3',
     id: uuidv4(),
-    text: 'Shema is important',
+    text: 'Hooks are nice',
     likes: 15,
   },
 ];
